7c1a62582d7be0dc8ec038b01e1ac461d716b0c1
Carlos Victorio <victorionogueira@email.com>
2026-01-09 13:48:44 -0300

    chore: remove unused React import

--- front/todo/src/pages/LoadingPage.jsx
+++ front/todo/src/pages/LoadingPage.jsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const LoadingPage = () => {
     return (
         <div className="w-screen h-screen flex justify-center items-center bg-blue-300">
--- front/todo/src/pages/NotFoundPage.jsx
+++ front/todo/src/pages/NotFoundPage.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import Alerta from "../assets/alerta.png";
 
 const NotFoundPage = () => {
