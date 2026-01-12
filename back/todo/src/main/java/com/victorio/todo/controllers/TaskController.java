d84a0fca74bba3d01699d4d576634a8529313491
Carlos Victorio <victorionogueira@email.com>
2026-01-12 18:28:18 -0300

    feat: document endpoints with Swagger and add JWT security requirement

--- back/todo/src/main/java/com/victorio/todo/controllers/TaskController.java
+++ back/todo/src/main/java/com/victorio/todo/controllers/TaskController.java
@@ -5,6 +5,8 @@ import com.victorio.todo.dto.task.TaskResponseDto;
 import com.victorio.todo.dto.task.TaskUpdateNameDto;
 import com.victorio.todo.dto.task.TaskUpdateStatusDto;
 import com.victorio.todo.services.TaskService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@SecurityRequirement(name = "bearerAuth")
 @RestController
 @RequestMapping("/tasks")
 public class TaskController {
@@ -23,12 +26,18 @@ public class TaskController {
         this.taskService = taskService;
     }
 
+    @Operation(
+            summary = "Create task"
+    )
     @PostMapping
     public ResponseEntity<TaskResponseDto> createTask(@AuthenticationPrincipal Long userId, @RequestBody @Valid TaskCreateDto taskCreateDto) {
         TaskResponseDto createdTask = taskService.createTask(userId, taskCreateDto);
         return ResponseEntity.status(HttpStatus.CREATED).body(createdTask);
     }
 
+    @Operation(
+            summary = "Get all tasks by logged-in user"
+    )
     @GetMapping
     public ResponseEntity<List<TaskResponseDto>> getTasksByUser (@AuthenticationPrincipal Long userId) {
         List<TaskResponseDto> tasks = taskService.findTasksByUserId(userId);
@@ -36,6 +45,10 @@ public class TaskController {
     }
 
     //Verificar se só ADMINS tem acesso a essa rota
+    @Operation(
+            summary = "Get task by its ID",
+            description = "Gets task of the logged-in user by Task ID"
+    )
     @GetMapping("/{taskId}")
     public ResponseEntity<TaskResponseDto> getTaskById (@AuthenticationPrincipal Long userId, @PathVariable Long taskId) {
         TaskResponseDto taskFound = taskService.findTaskById(userId, taskId);
@@ -43,18 +56,39 @@ public class TaskController {
 
     }
 
+    @Operation(
+            summary = "Update tasks name by taskId",
+            description = """
+                    Updates name of a task identified by its ID <br>
+                    The task belongs to the currently logged-in user
+                    """
+    )
     @PatchMapping("/{taskId}/name")
     public ResponseEntity<TaskResponseDto> updateTaskName(@AuthenticationPrincipal Long userId, @PathVariable Long taskId, @RequestBody TaskUpdateNameDto name) {
         TaskResponseDto taskUpdated = taskService.updateTaskName(userId, taskId, name);
         return ResponseEntity.status(HttpStatus.OK).body(taskUpdated);
     }
 
+    @Operation(
+            summary = "Update tasks status by taskId",
+            description = """
+                    Updates status of a task identified by its ID <br>
+                    The task belongs to the currently logged-in user
+                    """
+    )
     @PatchMapping("/{taskId}/status")
     public ResponseEntity<TaskResponseDto> updateTaskStatus(@AuthenticationPrincipal Long userId, @PathVariable Long taskId,  @RequestBody TaskUpdateStatusDto statusDto) {
         TaskResponseDto taskUpdated = taskService.updateTaskStatus(userId, taskId, statusDto);
         return ResponseEntity.status(HttpStatus.OK).body(taskUpdated);
     }
 
+    @Operation(
+            summary = "Delete tasks by taskId",
+            description = """
+                    Deletes a task identified by its ID <br>
+                    The task belongs to the currently logged-in user
+                    """
+    )
     @DeleteMapping("/{taskId}")
     public ResponseEntity<Void> deleteTask(@AuthenticationPrincipal Long userId, @PathVariable Long taskId) {
         taskService.deleteTask(userId, taskId);
--- back/todo/src/main/java/com/victorio/todo/controllers/UserController.java
+++ back/todo/src/main/java/com/victorio/todo/controllers/UserController.java
@@ -4,6 +4,8 @@ import com.victorio.todo.dto.user.UserCreateDto;
 import com.victorio.todo.dto.user.UserResponseDto;
 import com.victorio.todo.dto.user.UserUpdateDto;
 import com.victorio.todo.services.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@SecurityRequirement(name = "bearerAuth")
 @RestController
 @RequestMapping("/users")
 public class UserController {
@@ -22,53 +25,94 @@ public class UserController {
         this.userService = userService;
     }
 
+    @Operation(
+            summary = "Create User",
+            description = "*Restricted to ADMIN* <br>"
+    )
     @PostMapping
     public ResponseEntity<UserResponseDto> createUser(@RequestBody @Valid UserCreateDto userCreateDto) {
         UserResponseDto userResponse = userService.createUser(userCreateDto);
         return ResponseEntity.status(HttpStatus.CREATED).body(userResponse);
     }
 
+    @Operation(
+            summary = "Test it"
+    )
     @GetMapping("/test")
     public ResponseEntity<String> test() {
         return ResponseEntity.status(HttpStatus.OK).body("Funcionou!");
     }
 
+    @Operation(
+            summary = "Get all users",
+            description = "*Restricted to ADMIN* <br>"
+    )
     @GetMapping
     public ResponseEntity<List<UserResponseDto>> getAllUsers() {
         List <UserResponseDto> allUsers = userService.findAllUsers();
         return ResponseEntity.status(HttpStatus.OK).body(allUsers);
     }
 
+    @Operation(
+            summary = "Get user by ID",
+            description = """
+                    *Restricted to ADMIN* <br>
+                    Gets any user by their ID
+                    """
+    )
     @GetMapping("/{id}")
     public ResponseEntity<UserResponseDto> getUserById(@PathVariable Long id) {
         UserResponseDto user = userService.findUserById(id);
         return ResponseEntity.status(HttpStatus.OK).body(user);
     }
 
+    @Operation(
+            summary = "Get logged-in user"
+    )
     @GetMapping("/me")
     public ResponseEntity<UserResponseDto> getMe(@AuthenticationPrincipal Long userId) {
         UserResponseDto userLogged = userService.findUserById(userId);
         return ResponseEntity.status(HttpStatus.OK).body(userLogged);
     }
 
+    @Operation(
+            summary = "Update user by ID",
+            description = """
+                    *Restricted to ADMIN* <br>
+                    Updates any user by their ID
+                    """
+    )
     @PatchMapping("/{id}")
     public ResponseEntity<UserResponseDto> updateUserById(@PathVariable Long id, @RequestBody @Valid UserUpdateDto userUpdateDto) {
         UserResponseDto updatedUser = userService.updateUser(id, userUpdateDto);
         return ResponseEntity.status(HttpStatus.OK).body(updatedUser);
     }
 
+    @Operation(
+            summary = "Update logged-in user"
+    )
     @PatchMapping("/me")
     public ResponseEntity<UserResponseDto> updateMe(@AuthenticationPrincipal Long userId, @RequestBody UserUpdateDto data) {
         UserResponseDto userUpdated = userService.updateUser(userId, data);
         return ResponseEntity.status(HttpStatus.OK).body(userUpdated);
     }
 
+    @Operation(
+            summary = "Delete user by ID",
+            description = """
+                    *Restricted to ADMIN* <br>
+                    Deletes any user by their ID
+                    """
+    )
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteUserById(@PathVariable Long id) {
         userService.deleteUser(id);
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 
+    @Operation(
+            summary = "Delete logged-in user"
+    )
     @DeleteMapping("/me")
     public ResponseEntity<Void> deleteMe(@AuthenticationPrincipal Long UserId) {
         userService.deleteUser(UserId);
