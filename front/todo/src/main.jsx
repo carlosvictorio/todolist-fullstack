74e290634a281b8e4a9dafe2ebe84f3c11a17afe
Carlos Victorio <victorionogueira@email.com>
2026-01-23 11:55:32 -0300

    feat: redirect root route to /todolist

--- front/todo/src/main.jsx
+++ front/todo/src/main.jsx
@@ -2,19 +2,20 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import TodoList from "./pages/TodoList";
 import "./main.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+    createBrowserRouter,
+    RouterProvider,
+    Navigate,
+} from "react-router-dom";
 import Login from "./pages/Login";
 import NotFoundPage from "./pages/NotFoundPage";
 import Register from "./pages/Register";
 import { AuthProvider } from "./contexts/AuthContext";
 import ProtectedRoute from "./routes/ProtectedRoute";
-import LoadingPage from "./pages/LoadingPage";
-import { Header } from "./components/Header/Header";
 import EditPage from "./pages/EditPage";
 
 const router = createBrowserRouter([
-    { path: "/header", element: <Header /> }, //Testando
-
+    { path: "/", element: <Navigate to="/todolist" replace /> },
     { path: "/register", element: <Register /> },
     { path: "/login", element: <Login /> },
     { path: "*", element: <NotFoundPage /> },
@@ -44,5 +45,5 @@ const router = createBrowserRouter([
 createRoot(document.getElementById("root")).render(
     <StrictMode>
         <RouterProvider router={router} />
-    </StrictMode>
+    </StrictMode>,
 );
